51c61f035a19ff1aa73b9984fefece571c3dcb41
NaimBijapure <naim@vinnovtetechnologies.com>
2019-09-22 20:39:41 +0530

    update

--- Projects/todoify-client/src/helpers/reqHandler.js
+++
@@ -1,8 +0,0 @@
-import Store from '@/store/store'
-export default (config) => {
-  Store.commit('showRequestStatus')
-  if (Store.getters.isAuth()) {
-    config.headers.Authorization = Store.getters.getAccessToken
-  }
-  return config
-}
--- Projects/todoify-client/src/helpers/resHandler.js
+++
@@ -1,5 +0,0 @@
-import Store from '@/store/store'
-export default (response) => {
-  Store.commit('hideRequestStatus')
-  return response
-}
